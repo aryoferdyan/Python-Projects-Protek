ea3ecc4226b3beab1cfcbfaa1f9d1b206678cfde
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2021-06-22 21:43:34 +0700

    up

--- Praktikum 10/latihan1.py
+++ Praktikum 10/latihan1.py
@@ -1,26 +1,12 @@
-import os, sys
+jumlah = int(input('Jumlah'))
+kat = int(input('Kategori'))
 
-try:
-    #file = open("D:/UNS/Github/Python Projects/Praktikum 10/1.txt",'r')
-    
-    #input direktori sama dengan folder kode
-    file = open(os.path.join(sys.path[0], "1.txt"), "r")
+if kat == '1':
+    harga = 255000
+if kat == '2':
+    harga = 150000
+if kat == '3':
+    harga = 75000
 
-    gjl = 0
-    gnp = 0
-
-    for i in file.readlines():
-        x = int(i)
-        if x == 0:
-            continue
-        if x % 2 == 0:
-            gnp += 1
-        if x % 2 == 1:
-            gjl += 1
-
-    print("Banyaknya bilangan genap  :",gnp)
-    print("Banyaknya bilangan ganjil :",gjl)
-
-except FileNotFoundError:
-
-    print('\n File tidak ditemukan coba direktori lain \n')
+total = jumlah*harga
+print(total)
